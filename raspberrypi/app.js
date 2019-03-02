eb9dd285db9fab53a19e8812281898299df54b05
Carlos Vélez García <cvg25@alu.ua.es>
2019-03-02 18:47:53 +0100

    Bug fix

--- raspberrypi/app.js
+++ raspberrypi/app.js
@@ -1,11 +1,37 @@
 var express = require('express')
 var app = express();
-var router = require('./router')
+var Beacons = require('./beacons')
 var bodyParser = require('body-parser');
 const PORT = 3000;
 
-app.use(bodyParser.json());
-app.use('/', router)
+app.use(bodyParser.json())
+
+//Detiene el beacon si está activo.
+app.delete('/beacon', (req, res) => {
+    var exito = Beacons.EddystoneBeacon.stop();
+    if (exito) {
+        res.status(200).send('Beacon parado')
+    } else {
+        res.status(409).send('No hay ningun beacon activo')
+    }
+})
+
+//Crea el beacon si no está activo.
+app.post('/beacon', (req, res) => {
+
+    var configuration = req.body;
+    if (configuration.namespaceID && configuration.instanceID) {
+        var exito = Beacons.EddystoneBeacon.advertiseUID(configuration.namespaceID, configuration.instanceID, configuration.options)
+
+        if (exito) {
+            res.status(201).send('Beacon activo!')
+        } else {
+            res.status(409).send('Ya hay un beacon activo, debes pararlo antes de activar otro: DELETE /beacon')
+        }
+    } else {
+        res.status(400).send('Bad request, los campos: namespaceID y instanceID son obligatorios');
+    }
+})
 
 app.get('*', (pet, res) => {
     res.status(200)
